618a3f0cc41dcaea41581db3c184d71af4219ec5
keshavsoft <nknnkumar@yahoo.com>
2026-04-28 08:53:09 +0530

    npm start

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,7 @@
-export interface TableQueryAPI {
-  findAll: (...args: any[]) => any;
-  findByPk: (...args: any[]) => any;
-}
+// index.d.ts
 
-export interface TableMutateAPI {
-  insertWithChecks: (...args: any[]) => any;
-  insertGenPk: (...args: any[]) => any;
-}
+export function sendToTally(Id: string): Promise<any>;
 
-export interface TableAPI {
-  query: TableQueryAPI;
-  mutate: TableMutateAPI;
-}
+export function ledger(): Promise<any>;
 
-export interface KSchema {
-  loadConfig: (config: any) => void;
-  getConfig: () => any;
-  table: (tableName: string) => TableAPI;
-}
-
-export const kschema: KSchema;
-export const schemaMeta: any;
-export const argsSchema: any;
-export const descSchema: any;
-export const exampleSchema: any;
+export function stockItems(): Promise<any>;
